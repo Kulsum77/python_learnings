4559911e9b5338d32d1ef36d6538e1064e5cc07e
Kulsum77 <kulsumumma619@gmail.com>
2025-07-08 15:11:19 +0530

    planet

---
+++ pluto.py
@@ -0,0 +1,9 @@
+""" 
+This program implements that the planet list occur pluto or not
+"""
+
+planet_list=input("enter a list of planets")
+if "pluto" in planet_list:
+    print("pluto is in the list")
+else:
+    print("pluto is not in the list")

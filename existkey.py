e4833276783751805e294a9f6383d75350caecc0
Kulsum77 <kulsumumma619@gmail.com>
2025-07-08 16:12:42 +0530

    key_exist

---
+++ existkey.py
@@ -0,0 +1,9 @@
+"""
+This program implements that the key exist in dictionary 
+"""
+a=input("enter key:")
+dict_1={"name":"osborn","age":22}
+if a in dict_1:
+    print("key exist")
+else:
+    print("key does not exist")

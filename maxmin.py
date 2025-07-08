47eeaa7b4219e4a42c9a05d6030f133886f2f772
Kulsum77 <kulsumumma619@gmail.com>
2025-07-08 16:08:41 +0530

    maximum_minimum

---
+++ maxmin.py
@@ -0,0 +1,10 @@
+"""
+This program implements whether the number is max or min 
+"""
+my_dict = {'a': 65, 'b': 21, 'c': 98, 'd': 77}
+
+max_value = max(my_dict.values())
+min_value = min(my_dict.values())
+
+print("Maximum value:", max_value)
+print("Minimum value:", min_value)

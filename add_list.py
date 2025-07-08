12d04da951d4324f12dbdbcea29cbfdcac3280c7
Kulsum77 <kulsumumma619@gmail.com>
2025-07-08 16:04:53 +0530

    sum_of_numbers

--- add_list.py
+++ add_list.py
@@ -5,8 +5,9 @@ This program implements that when the user gave a number it will add till the nu
 num = int(input("Enter the number : "))
 add = 0
 i = 1
-while i<=num:
-    add+=i
-    i+=1
+while i<=num:a = [17, 28, 32, 46]
+total = 0
+for num in a:
+    total += num
     
 print("The total number added is :",add)  
---
+++ sum.py
@@ -0,0 +1,8 @@
+"""
+This program is used to calculate the sum of all items
+"""
+a = [17, 28, 32, 46]
+total = 0
+for num in a:
+    total += num
+print(total)

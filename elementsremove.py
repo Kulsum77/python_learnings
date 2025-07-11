41bbd342fc970f8a1501f1505becae3293a5e385
Kulsum77 <kulsumumma619@gmail.com>
2025-07-11 11:38:24 +0530

    Delete elementsremove.py

--- elementsremove.py
+++
@@ -1,8 +0,0 @@
-"""
-This program implements that the list is removed from the element
-"""
-mylist=["black","white","pink","violet","yellow","red","blue","brown"]
-result = []
-for i in range(len(mylist)):
-    if i != 0 and i != 4 and i != 5:
-        result.append(mylist[i])
